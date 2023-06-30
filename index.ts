af6cb2786319c349ef14d500d9d83b4b5cbdac56
Sn0wFR <ferreira.mathieu.pro@gmail.com>
2023-06-30 16:34:48 +0200

    update for second semester Rapport

--- index.ts
+++ index.ts
@@ -685,6 +685,18 @@ client.on('messageCreate', async (message) => {
                 let pointToRemove = 0
                 for (let i = 8; i < 29; i++) { // remove point before second semester so we still get bonus point
                     if (row[i] != 'X') {
+                        let ligne = row[i].split(" ");
+                        if (ligne[0].length == 2) {
+                            if (parseInt(ligne[0].substring(0, 1)) == 0) {
+                                if (ligne[1].length == 2) {
+                                    continue;
+                                } else {
+                                    if (parseInt(ligne[1].substring(0, 2)) < 30) {
+                                        continue;
+                                    }
+                                }
+                            }
+                        }
                         console.log(row[i]);
                         pointToRemove = pointToRemove + 1; //participation before second semester
                     }
